bc4c493d17ff87942d6b87fd3c07f97cda37f653
Fotios Zachopoulos <65657774+zachfotis@users.noreply.github.com>
2022-11-24 20:37:02 +0000

    fix typo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,14 +6,14 @@ function Navbar() {
     { name: 'Contact Us', link: '/' },
   ];
   return (
-    <div className='shadow-md w-full fixed top-0 left-0'>
-      <div className='md:flex items-center justify-between bg-blue py-4 md:px-10 p-7'>
-        <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-white'>Logo</div>
+    <div className="shadow-md w-full fixed top-0 left-0">
+      <div className="md:flex items-center justify-between bg-blue py-4 md:px-10 p-7">
+        <div className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-white">Logo</div>
 
-        <ul className='md:flex md:items-center text-white space-x-20'>
+        <ul className="md:flex md:items-center text-white space-x-20">
           {Links.map((link) => (
-            <li key={link.name} className='md:ml-11 text-xl cursor-pointer'>
-              <a hrfe={link.link} className='hover:text-yellow'>
+            <li key={link.name} className="md:ml-11 text-xl cursor-pointer">
+              <a hre={link.link} className="hover:text-yellow">
                 {link.name}
               </a>
             </li>
