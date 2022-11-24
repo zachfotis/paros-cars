0e1f13d31651a278204c727425e207a1bc2516ff
Fotios Zachopoulos <65657774+zachfotis@users.noreply.github.com>
2022-11-24 20:15:01 +0000

    add tailwind defaults

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {},
+      screens: {
+        mobile: { max: '450px' },
+        tablet: { max: '768px' },
+        laptop: { max: '1024px' },
+        laptopLG: { max: '1200px' },
+        desktop: { max: '1440px' },
+      },
+    },
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+      Montserrat: ['Montserrat', 'sans-serif'],
+    },
   },
   plugins: [],
 };
