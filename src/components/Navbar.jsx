966065927faccb469f6e8950174c482c780ee611
Chernobyl95 <13-13212@saeinstitute.edu>
2022-11-24 21:05:56 +0100

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,26 @@
 function Navbar() {
-  const page = 'Contact';
-
+  let Links = [
+    { name: 'Home', link: '/' },
+    { name: 'Cars', link: '/' },
+    { name: 'Rent Terms', link: '/' },
+    { name: 'Contact Us', link: '/' },
+  ];
   return (
-    <nav>
-      <div className='logo'>logo</div>
-      <ul>
-        <li className=''>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-        <li>{page}</li>
-      </ul>
-    </nav>
+    <div className='shadow-md w-full fixed top-0 left-0'>
+      <div className='md:flex items-center justify-between bg-blue py-4 md:px-10 p-7'>
+        <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-white'>Logo</div>
+
+        <ul className='md:flex md:items-center text-white space-x-20'>
+          {Links.map((link) => (
+            <li key={link.name} className='md:ml-11 text-xl cursor-pointer'>
+              <a hrfe={link.link} className='hover:text-yellow'>
+                {link.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,17 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      darkBlue: '#001E6C',
+      blue: '#035397',
+      orange: '#E8630A',
+      yellow: '#FCD900',
+      white: '#FFFFFF',
+      black: '#000000',
+    },
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+    },
   },
   plugins: [],
 };
