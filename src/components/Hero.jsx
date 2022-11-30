08a5689c52952509bb749688261875a257f76c30
Fotios Zachopoulos <65657774+zachfotis@users.noreply.github.com>
2022-11-30 22:42:40 +0200

    start Lookup component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,12 @@
 import HeroImage from '../assets/images/hero.webp';
+import Lookup from './Lookup';
 
 function Hero() {
-  return <img src={HeroImage} alt="hero" className="h-screen w-auto object-cover brightness-75 contrast-75" />;
+  return (
+    <>
+      <img src={HeroImage} alt="hero" className="relative h-screen w-auto object-cover brightness-75 contrast-75" />
+      <Lookup />
+    </>
+  );
 }
 export default Hero;
---
+++ src/components/Lookup.jsx
@@ -0,0 +1,16 @@
+import './Lookup.css';
+
+function Lookup() {
+  return (
+    <div className="lookup absolute z-10 top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-start items-start p-4">
+      <h1 className="text-xl">Check Availability</h1>
+      <div>
+        {/* //TODO: Pickup Place */}
+        {/* //TODO: Pickup Date */}
+        {/* //TODO: Return Place */}
+        {/* //TODO: Return Date */}
+      </div>
+    </div>
+  );
+}
+export default Lookup;
