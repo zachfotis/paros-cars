9eba98686d84d98eaa274010c81ade56b47043b9
Fotios Zachopoulos <65657774+zachfotis@users.noreply.github.com>
2022-11-30 14:30:27 +0000

    fix firebase env

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Admin from './pages/Admin';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <FirebaseProvider>
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/admin" element={<Admin />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,8 @@
 import Hero from '../components/Hero';
-import Navbar from '../components/Navbar';
 
 function Home() {
   return (
     <div className="min-h-screen flex flex-col justify-start items-start">
-      <Navbar />
       <Hero />
     </div>
   );
