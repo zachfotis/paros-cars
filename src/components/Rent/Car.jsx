810df2ec409cbb3b45073af40e26c131bc196024
Fotios Zachopoulos <65657774+zachfotis@users.noreply.github.com>
2022-12-23 23:06:59 +0200

    add cars

--- src/components/Rent/Car.jsx
+++ src/components/Rent/Car.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import useRentalContext from '../../context/RentalContext';
 import { Button } from '@mui/material';
 import CarImage from '../../assets/images/test_car.jpg';
@@ -8,15 +9,33 @@ import TransmissionIcon from '../../assets/icons/services/manual-transmission.sv
 import AirConditionIcon from '../../assets/icons/services/snowflake.svg';
 import { TfiDashboard } from 'react-icons/tfi';
 import { motion } from 'framer-motion';
+import { getStorage, ref, getDownloadURL } from 'firebase/storage';
+import { AiOutlineBgColors } from 'react-icons/ai';
 
-function Car({ car, totalDays = 1 }) {
+function Car({ car, totalDays = 1, showPrice = true, showAllOptions = true }) {
   const { setSelectedCar } = useRentalContext();
+  const [imageURL, setImageURL] = useState('');
+
   const passengers = car?.passengers > 1 ? `${car.passengers - 1} - ${car.passengers}` : car.passengers;
   const bags = car?.bags > 1 ? `${car.bags - 1} - ${car.bags}` : car.bags;
 
   const handleSelect = () => {
     setSelectedCar(car);
   };
+
+  useEffect(() => {
+    const getImageURL = async () => {
+      try {
+        const storage = getStorage();
+        const url = await getDownloadURL(ref(storage, car.url));
+        setImageURL(url);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getImageURL();
+  }, [car.url]);
 
   return (
     <motion.div
@@ -25,12 +44,12 @@ function Car({ car, totalDays = 1 }) {
       transition={{ duration: 0.5 }}
       exit={{ opacity: 0, x: '-100%' }}
       className="w-full max-w-[1280px] flex justify-center items-stretch gap-5 shadow-lg rounded-lg overflow-hidden p-5 border border-1 border-slate-300 bg-white
-    tablet:flex-wrap laptop:gap-2 tablet:py-5 tablet:px-2 mobile:flex-col mobile:items-center 
+      tablet:flex-wrap laptop:gap-2 tablet:py-5 tablet:px-2 mobile:flex-col mobile:items-center 
     "
     >
       {/* car image */}
-      <div className="w-[300px] p-2 laptop:w-[200px]">
-        <img src={CarImage} alt="car" className="w-full h-auto" />
+      <div className="w-[200px] p-2 laptop:w-[200px] flex justify-center items-center">
+        {imageURL && <img src={imageURL} alt="car" className="w-full h-auto" />}
       </div>
       {/* car info */}
       <div className="flex-1 flex flex-col justify-evenly items-stretch gap-5 py-2 px-4 laptop:px-5">
@@ -40,16 +59,8 @@ function Car({ car, totalDays = 1 }) {
         </div>
         <div className="w-full flex justify-start items-start flex-wrap gap-10 laptop:gap-5 tablet:justify-center">
           <div className="flex justify-center items-center gap-2">
-            <img src={PassengersIcon} alt="car door" className="w-[25px]" />
-            <p className="text-sm">{passengers}</p>
-          </div>
-          <div className="flex justify-center items-center gap-2">
             <img src={CarDoorIcon} alt="car door" className="w-[25px]" />
             <p className="text-sm">{car?.doors}</p>
-          </div>
-          <div className="flex justify-center items-center gap-2">
-            <img src={BagsIcon} alt="car door" className="w-[25px]" />
-            <p className="text-sm">{bags}</p>
           </div>
           <div className="flex justify-center items-center gap-2">
             <img src={TransmissionIcon} alt="car door" className="w-[25px]" />
@@ -60,6 +71,12 @@ function Car({ car, totalDays = 1 }) {
               <img src={AirConditionIcon} alt="car door" className="w-[25px]" />
             </div>
           )}
+          {showAllOptions && (
+            <div className="flex justify-center items-center gap-2">
+              <AiOutlineBgColors className="text-3xl" />
+              <p className="text-sm">{car?.color}</p>
+            </div>
+          )}
         </div>
         <div className="flex justify-start items-center gap-2 mobile:justify-center">
           <TfiDashboard className="text-2xl" />
@@ -67,22 +84,31 @@ function Car({ car, totalDays = 1 }) {
         </div>
       </div>
       {/* price info */}
-      <div className="w-fit flex flex-col justify-center items-center gap-5 py-5 px-10 laptop:px-5 tablet:w-full">
-        <div className="flex justify-center items-stretch gap-3">
-          <div className="flex flex-col justify-evenly items-center">
-            <p className="text-xl font-[400] text-gray-500">€ {car.price.toFixed(2)}</p>
-            <p className="text-xs font-[400] text-gray-500">Daily Rate</p>
+      {showPrice && (
+        <div className="w-fit flex flex-col justify-center items-center gap-5 py-5 px-10 laptop:px-5 tablet:w-full">
+          <div className="flex justify-center items-stretch gap-3">
+            <div className="flex flex-col justify-evenly items-center">
+              <p className="text-xl font-[400] text-gray-500">N/A</p>
+              <p className="text-xs font-[400] text-gray-500">Daily Rate</p>
+              {/* <p className="text-xl font-[400] text-gray-500">€ {car.price.toFixed(2)}</p>
+            <p className="text-xs font-[400] text-gray-500">Daily Rate</p> */}
+            </div>
+            <span className="w-[1px] h-[50px] bg-gray-200"></span>
+            <div className="flex flex-col justify-evenly items-center">
+              <p className="text-2xl font-[500] text-gray-500">N/A</p>
+              <p className="text-xs font-[400] text-gray-500">Total Amount</p>
+              {/* <p className="text-2xl font-[500] text-gray-500">€ {(totalDays * car.price).toFixed(2)}</p>
+            <p className="text-xs font-[400] text-gray-500">Total Amount</p> */}
+            </div>
           </div>
-          <span className="w-[1px] h-[50px] bg-gray-200"></span>
-          <div className="flex flex-col justify-evenly items-center">
-            <p className="text-2xl font-[500] text-gray-500">€ {(totalDays * car.price).toFixed(2)}</p>
-            <p className="text-xs font-[400] text-gray-500">Total Amount</p>
-          </div>
-        </div>
-        <Button variant="contained" color="primary" className="w-full" onClick={handleSelect}>
+          {/* <Button variant="contained" color="primary" className="w-full" onClick={handleSelect}>
           Select Vehicle
-        </Button>
-      </div>
+        </Button> */}
+          <Button disabled={true} variant="contained" color="primary" className="w-full" onClick={handleSelect}>
+            Soon Available
+          </Button>
+        </div>
+      )}
     </motion.div>
   );
 }
--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -1,17 +1,67 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { getFirestore, collection, addDoc, getDocs } from 'firebase/firestore';
 
-import ContactImage from '../assets/images/contact.jpg';
+import CarsImage from '../assets/images/CarsImage.jpg';
 import SoonAvailable from '../components/SoonAvailable';
 import PageLayout from '../components/Layout/PageLayout';
+import { RentalProvider } from '../context/RentalContext';
+import { motion, AnimatePresence } from 'framer-motion';
+import Results from '../components/Rent/Results';
+import Car from '../components/Rent/Car';
+import { toast } from 'react-toastify';
+import Loader from '../components/Loader';
 
 function Cars() {
+  const [availableCars, setAvailableCars] = useState([]);
+
   useEffect(() => {
     window.scrollTo(0, 0);
+
+    const getCars = async () => {
+      try {
+        const db = getFirestore();
+        const collectionRef = collection(db, 'cars');
+        const querySnap = await getDocs(collectionRef);
+        const cars = querySnap.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+        setAvailableCars(cars);
+      } catch (error) {
+        toast.error('Παρουσιάστηκε κάποιο πρόβλημα');
+      }
+    };
+
+    getCars();
   }, []);
 
-  return (
-    <PageLayout title={layoutText} image={ContactImage}>
-      <SoonAvailable />
+  // const handleUpload = () => {
+  //   const uploadCars = async () => {
+  //     try {
+  //       const db = getFirestore();
+  //       const collectionRef = collection(db, 'cars');
+  //       cars.forEach(async (car) => {
+  //         await addDoc(collectionRef, car);
+  //       });
+  //       console.log('Cars uploaded');
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+
+  //   uploadCars();
+  // };
+
+  return availableCars.length === 0 ? (
+    <Loader />
+  ) : (
+    <PageLayout title={layoutText} image={CarsImage}>
+      <AnimatePresence>
+        <div className="w-full max-w-[1280px] flex justify-center items-stretch flex-wrap gap-10">
+          {availableCars.map((car) => (
+            <motion.div className="w-[40%] laptopLG:w-[90%]" whileHover={{ scale: 1.05 }}>
+              <Car key={car.id} car={car} showPrice={false} showAllOptions={false} />
+            </motion.div>
+          ))}
+        </div>
+      </AnimatePresence>
     </PageLayout>
   );
 }
@@ -21,3 +71,196 @@ const layoutText = {
   EN: 'Our Cars',
   GR: 'Τα Αυτοκίνητα Μας',
 };
+
+const cars = [
+  {
+    airCondition: true,
+    bags: 5,
+    bookedDates: [],
+    doors: 5,
+    mileage: 'Unlimited',
+    name: 'Ford Fiesta',
+    passengers: 5,
+    price: 30,
+    transmission: 'M',
+    year: 2015,
+    color: 'Silver',
+    url: 'gs://parodise-cars.appspot.com/cars/ford-fiesta.png',
+  },
+  {
+    airCondition: true,
+    bags: 5,
+    bookedDates: [],
+    doors: 5,
+    mileage: 'Unlimited',
+    name: 'Renault Clio',
+    passengers: 5,
+    price: 30,
+    transmission: 'M',
+    year: 2016,
+    color: 'Red',
+    url: 'gs://parodise-cars.appspot.com/cars/renault-clio.png',
+  },
+  {
+    airCondition: true,
+    bags: 5,
+    bookedDates: [],
+    doors: 5,
+    mileage: 'Unlimited',
+    name: 'Toyota Aygo',
+    passengers: 5,
+    price: 30,
+    transmission: 'M',
+    year: 2016,
+    color: 'White',
+    url: 'gs://parodise-cars.appspot.com/cars/toyota-aygo.png',
+  },
+  {
+    airCondition: true,
+    bags: 5,
+    bookedDates: [],
+    doors: 5,
+    mileage: 'Unlimited',
+    name: 'Kia Piccanto',
+    passengers: 5,
+    price: 30,
+    transmission: 'M',
+    year: 2016,
+    color: 'Gold',
+    url: 'gs://parodise-cars.appspot.com/cars/kia-piccanto.png',
+  },
+  {
+    airCondition: true,
+    bags: 5,
+    bookedDates: [],
+    doors: 5,
+    mileage: 'Unlimited',
+    name: 'Chevrolet Aveo',
+    passengers: 5,
+    price: 30,
+    transmission: 'A',
+    year: 2015,
+    color: 'Black',
+    url: 'gs://parodise-cars.appspot.com/cars/chevrolet-aveo.png',
+  },
+  {
+    airCondition: true,
+    bags: 5,
+    bookedDates: [],
+    doors: 5,
+    mileage: 'Unlimited',
+    name: 'Citroen C1',
+    passengers: 5,
+    price: 30,
+    transmission: 'A',
+    year: 2014,
+    color: 'Grafiti',
+    url: 'gs://parodise-cars.appspot.com/cars/citroen-c1.png',
+  },
+  {
+    airCondition: true,
+    bags: 5,
+    bookedDates: [],
+    doors: 5,
+    mileage: 'Unlimited',
+    name: 'Renault Scenic',
+    passengers: 5,
+    price: 30,
+    transmission: 'M',
+    year: 2012,
+    color: 'Brown',
+    url: 'gs://parodise-cars.appspot.com/cars/renault-scenic.png',
+  },
+  {
+    airCondition: true,
+    bags: 5,
+    bookedDates: [],
+    doors: 5,
+    mileage: 'Unlimited',
+    name: 'Hundai i10',
+    passengers: 5,
+    price: 30,
+    transmission: 'M',
+    year: 2014,
+    color: 'Red',
+    url: 'gs://parodise-cars.appspot.com/cars/hundai-i10.png',
+  },
+  {
+    airCondition: true,
+    bags: 5,
+    bookedDates: [],
+    doors: 5,
+    mileage: 'Unlimited',
+    name: 'Fiat Panda',
+    passengers: 5,
+    price: 30,
+    transmission: 'M',
+    year: 2014,
+    color: 'Red',
+    url: 'gs://parodise-cars.appspot.com/cars/fiat-panda.png',
+  },
+  {
+    airCondition: true,
+    bags: 5,
+    bookedDates: [],
+    doors: 5,
+    mileage: 'Unlimited',
+    name: 'Fiat Grande Punto',
+    passengers: 5,
+    price: 30,
+    transmission: 'M',
+    year: 2013,
+    color: 'Black',
+    url: 'gs://parodise-cars.appspot.com/cars/fiat-grande-punto.png',
+  },
+  {
+    airCondition: true,
+    bags: 5,
+    bookedDates: [],
+    doors: 5,
+    mileage: 'Unlimited',
+    name: 'Toyota Yaris',
+    passengers: 5,
+    price: 30,
+    transmission: 'A',
+    year: 2018,
+    color: 'Blue electric',
+    url: 'gs://parodise-cars.appspot.com/cars/toyota-yaris.png',
+  },
+  {
+    airCondition: true,
+    bags: 5,
+    bookedDates: [],
+    doors: 5,
+    mileage: 'Unlimited',
+    name: 'Citroen C3',
+    passengers: 5,
+    price: 30,
+    transmission: 'M',
+    year: 2016,
+    color: 'Black',
+    url: 'gs://parodise-cars.appspot.com/cars/citroen-c3.png',
+  },
+  {
+    airCondition: true,
+    bags: 5,
+    bookedDates: [],
+    doors: 5,
+    mileage: 'Unlimited',
+    name: 'Toyota Yaris',
+    passengers: 5,
+    price: 30,
+    transmission: 'M',
+    year: 2018,
+    color: 'Brown',
+    url: 'gs://parodise-cars.appspot.com/cars/toyota-yaris.png',
+  },
+];
+
+const bookedDates = [
+  {
+    endDate: 'December 6, 2022 at 12:00:00 AM UTC+2',
+    startDate: 'December 3, 2022 at 12:00:00 AM UTC+2',
+    status: 'booked',
+  },
+];
